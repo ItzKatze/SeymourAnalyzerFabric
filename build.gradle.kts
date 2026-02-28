5c3405fda29eec3a5ce366f89fe9f37a6244bba5
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2026-02-28 10:44:37 +0100

    bump java to 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,14 +48,18 @@ tasks.processResources {
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release.set(21)
+    options.release.set(25)
 }
 
 java {
     withSourcesJar()
 
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+
+    sourceCompatibility = JavaVersion.VERSION_25
+    targetCompatibility = JavaVersion.VERSION_25
 }
 
 tasks.jar {
